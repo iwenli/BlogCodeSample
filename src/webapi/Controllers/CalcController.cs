8b5090acc367f471e847d0e47ab9f9bb9bb50d98
c c <234486036@qq.com>
2020-05-30 01:30:10 +0800

    dotnet watch run sample

---
+++ src/webapi/Controllers/CalcController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [Route("[action]")]
+        public ActionResult<string> Sum(
+            [FromQuery(Name = "num1")] int num1,
+            [FromQuery(Name = "num2")] int num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"{num1} + {num2} = {sum}.";
+        }
+
+        [Route("[action]")]
+        public ActionResult<string> SumInts(
+            [FromQuery(Name = "num1")] int num1,
+            [FromQuery(Name = "num2")] int num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"[ints]  {num1} + {num2} = {sum}.";
+        }
+
+        [Route("[action]")]
+        public ActionResult<string> SumDoubles(
+            [FromQuery(Name = "num1")] double num1,
+            [FromQuery(Name = "num2")] double num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"[doubles]  {num1} + {num2} = {sum}.";
+        }
+    }
+}
